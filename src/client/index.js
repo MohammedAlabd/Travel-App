81e37013928cf92370f05187800294c20bbf2c73
MohammedAlabd <mu96abd@gmail.com>
2020-06-14 16:04:47 +0300

    use the country list

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,7 @@
 import { handleSubmit } from "./js/handleSubmit";
+import "./js/countriesAPI"
 // import { renderResults } from "./js/renderResult";
 // import "./styles/main.scss";
+
 
 export { handleSubmit };
---
+++ src/client/js/countriesAPI.js
@@ -0,0 +1,34 @@
+import countries from "country-list-js";
+
+// const names = countries.names()
+(function () {
+  const countriesArray = Object.keys(countries.all).map((key) => ({
+    iso2: countries.all[key]["iso2"],
+    name: countries.all[key]["name"],
+  }));
+
+  // sort by name
+  countriesArray.sort(function (a, b) {
+    var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+    var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+
+  console.log(countriesArray);
+  const renderCountries = () => {
+    const countriesList = document.querySelector("#countries-list");
+    countriesArray.forEach((country) => {
+      const countryHTMLOption = `<option value="${country.iso2}">${country.name}</option>`;
+      countriesList.insertAdjacentHTML("beforeEnd", countryHTMLOption);
+    });
+  };
+  document.addEventListener("DOMContentLoaded", renderCountries);
+})();

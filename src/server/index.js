a29c005f6b01a6194ecb2a0430b3578354d64d03
MohammedAlabd <mu96abd@gmail.com>
2020-06-14 16:07:35 +0300

    use the Helpers methods to get the right data and send it back to the client side

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
+// Helpers Methods
+const fetchWeather = require("./helperMethods/fetchWeather");
+const fetchCoordinates = require("./helperMethods/fetchCoordinates");
+const fetchImage = require("./helperMethods/fetchImage");
+
+// Constant Variables
+const USERNAME = "mohammedalabd";
+const BASE_COORDINATES_URL = "http://api.geonames.org/postalCodeSearchJSON";
+const WETHERBIT_API_KEY = "1774a820b2644d1e9b3a181d7e804837";
+const PIXABAY_API_KEY = "17037476-2956bf4cea34945d496b59932";
+const BASE_PIXABAY_URL = "https://pixabay.com/api";
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,8 +32,8 @@ app.get("/", function (req, res) {
 });
 
 // designates what port the app will listen to for incoming requests
-app.listen(8082, function () {
-  console.log("Example app listening on port 8081!");
+app.listen(8083, function () {
+  console.log("Example app listening on port 8083!");
 });
 
 app.get("/test", function (req, res) {
@@ -30,9 +42,26 @@ app.get("/test", function (req, res) {
 
 const appData = {};
 
-const analysisFunction = async (req, res) => {
-  console.log(appData);
-  res.send(JSON.stringify(appData));
+const main = async (req, res) => {
+  console.log(req.body);
+  const { country, city, date } = req.body;
+  const coordinates = await fetchCoordinates(
+    country,
+    city,
+    BASE_COORDINATES_URL,
+    USERNAME
+  );
+  if (!coordinates) {
+    res.send(JSON.stringify({ state: "error", msg: "can't find this city" }));
+    return;
+  }
+  const weatherData = await fetchWeather(coordinates, date, WETHERBIT_API_KEY);
+  let image = await fetchImage(city, BASE_PIXABAY_URL, PIXABAY_API_KEY);
+  if (!image) {
+    image = await fetchImage(coordinates.adminName2, BASE_PIXABAY_URL, PIXABAY_API_KEY);
+  }
+  res.send(JSON.stringify({ image, weatherData }));
+  console.log({ image, weatherData });
 };
 
-app.post("/add", analysisFunction);
+app.post("/post", main);

e7e9bf7d9111f81159f3632ea306b5223bebec1f
MohammedAlabd <mu96abd@gmail.com>
2020-06-15 12:40:07 +0300

    add event listener instead of the inline js

--- src/client/index.js
+++ src/client/index.js
@@ -3,7 +3,12 @@ import { renderTravelCard } from "./js/renderTravelCard";
 import "./js/countriesAPI"
 import "./styles/card.scss";
 
-const todayDate = new Date().toJSON().split("T")[0] // To get data in this format "YYYY-MM-DD"
-document.querySelector("#date-input").setAttribute("min", todayDate)
+document.addEventListener("DOMContentLoaded", () => {
+    const todayDate = new Date().toJSON().split("T")[0] // To get data in this format "YYYY-MM-DD"
+    document.querySelector("#date-input").setAttribute("min", todayDate)
+
+    document.querySelector("form").addEventListener("submit", handleSubmit)
+})
+
 
 export { handleSubmit, renderTravelCard };
